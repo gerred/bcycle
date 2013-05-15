945bcc99deebcb5a6fe51367703ce62104996f1e
Gerred Dillon <hello@gerred.com>
2013-05-15 09:28:51 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+/*jshint asi:true, laxcomma:true */
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , request = require('request')
+  , async = require('async')
+  , crc = require('crc')
+  , Firebase = require('firebase')
+  , stationsRef = new Firebase('https://bcycle.firebaseIO.com/stations');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('stylus').middleware(__dirname + "/public"));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+var checkBikes = function() {
+    request({url: 'http://api.bcycle.com/Services/Mobile.svc/ListKiosks', json: true}, function(err, res, body) {
+        body.d.list.map(function(stationJson) {
+            var station = new Station(stationJson, crc.crc32(JSON.stringify(stationJson)))
+
+            if (stationsRef.child(station.id).crc !== station.crc) {
+                if (typeof station.city !== undefined) {
+                    stationsRef.child(station.state).child(station.id).set(station)
+                }
+            }
+        })
+
+        setTimeout(checkBikes, 500)
+    })
+}
+
+setTimeout(checkBikes, 500)
+
+function Station(params, crc) {
+    this.crc = crc
+    this.city = params.Address.City
+    this.state = params.Address.State
+    this.name = params.Name.replace(/\s$/, "")
+    this.location = {lat: params.Location.Latitude, lon: params.Location.Longitude}
+    this.id = params.Id
+    this.bikesAvailable = params.BikesAvailable
+    this.active = params.Status === 'Active'
+}
---
+++ public/javascripts/app.js
@@ -0,0 +1,76 @@
+angular.module('bcycle', ['firebase'])
+  .controller('MainCtrl', ['$scope', 'angularFireCollection',
+    function MainCtrl($scope, angularFireCollection) {
+        var state = 'CO'
+        var markerLayer;
+        var map = L.map('map').setView([51.505, -0.09], 1);
+        map.locate({setView: true, maxZoom: 16});
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        navigator.geolocation.getCurrentPosition(function(position) {
+
+            var url = 'https://bcycle.firebaseio.com/stations/' + state
+            $scope.stations = angularFireCollection(url, function(stations) {
+                $scope.setupMarkers(stations)
+                stations.ref().on('child_changed', function(val) {
+                    val.val().changed = true
+                    $scope.setupMarkers(stations)
+                });
+            });
+
+            $scope.maxDistance = function(station) {
+                return $scope.distance(station) <= 20.0
+            }
+
+            $scope.setupMarkers = function(stations) {
+                var markers = [];
+                var stationsObj = stations.val();
+                for (var key in stationsObj) {
+                    if (stationsObj[key].active === true) {
+                        if (typeof markerLayer !== "undefined") { map.removeLayer(markerLayer); }
+                        if (typeof stationsObj[key].location !== "undefined") {
+                            var location = stationsObj[key].location;
+                            var bikeIcon = L.divIcon({
+                                html: stationsObj[key].bikesAvailable
+                            });
+                            var marker = new L.Marker([location.lat, location.lon], {
+                                icon: bikeIcon
+                            });
+                            markers.push(marker);
+                        }
+                    }
+                    
+                }
+                markerLayer = L.layerGroup(markers).addTo(map);
+            }
+
+
+            $scope.distance = function(station) {
+                    var stationLocation = station.location;
+                    var userLocation = position.coords;
+
+                    var lat1, lon1, lat2, lon2, dlat, dlon, a, c, dm;
+
+                    lat1 = deg2rad(userLocation.latitude);
+                    lon1 = deg2rad(userLocation.longitude);
+                    lat2 = deg2rad(stationLocation.lat);
+                    lon2 = deg2rad(stationLocation.lon);
+
+
+                    dlat = lat2 - lat1;
+                    dlon = lon2 - lon1;
+
+                    a = Math.pow(Math.sin(dlat/2),2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon/2),2);
+                    c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+
+                    return Math.round(c * 3961.0 * 1000)/1000;
+            }
+
+            function deg2rad(deg) {
+                return deg * Math.PI/180;
+            }
+    });
+
+    }
+]);
